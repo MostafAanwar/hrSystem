dd883e4c9e0062a6316b5ac88ded61858ce569b7
Lina Sabboula <linamuhab@gmail.com>
2019-12-12 22:30:51 +0200

    Added Model class with DB connection functions

--- model/model.js
+++ model/model.js
@@ -1,7 +1,37 @@
-// import db from "../db/db";
-//
-// module.exports = {
-//   getAllUsers: function() {
-//     return db;
-//   }
-// };
+let mysql = require('mysql');
+class Model{
+    connect() {
+            let connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'hrsystem'
+        });
+        connection.connect(function (error) {
+            if (error) {
+                return console.error('error: ' + error.message);
+            }
+            console.log("Connection established!");
+            return connection;
+
+        });
+    } // end connect
+    disconnect(connection){
+        connection.end()
+    }
+
+    viewHR(){
+        let connection = this.connect();
+        let query = 'SELECT * FROM hr';
+        connection.query(query,(error, results, fields) =>{
+            if(error){
+                return console.error(error.message);
+            }
+            console.log(results);
+        });
+        disconnect(connection);
+    }
+}
+
+const mainModel = new Model();
+export default mainModel;
