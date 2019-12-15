219ff6a7957adc92031007fb27f440a4d45469ed
Lina Sabboula <linamuhab@gmail.com>
2019-12-15 22:35:43 +0200

    Added getAllHR and getHR controller functions and respective endpoint URLs

--- controllers/controller.js
+++ controllers/controller.js
@@ -28,13 +28,31 @@ class Controller {
         });
     }
 
-    getAllUsers(req, res) {
-        let users = model.getAllUsers();
-        if (users) {
-            res.status(200).send({
-                users: users
-            });
-        }
+    getHR(req, res) {
+        model.getHR().then((response) => { //response contains returned data
+            res.contentType('json');
+            console.log(response.result);
+            res.send(response.result);
+            return response.connection; //returned on next then
+        }).then((con) => {
+            model.disconnect(con);
+        }).catch((err) => {
+            return console.error("Error: " + err.message);
+        });
+    }
+
+    getAllHR(req, res) {
+        console.log("controller");
+        model.getAllHR().then((response) => { //response contains returned data
+            res.contentType('json');
+            console.log(response.result);
+            res.send(response.result);
+            return response.connection; //returned on next then
+        }).then((con) => {
+            model.disconnect(con); //TODO
+        }).catch((err) => {
+            return console.error("Error! " + err.message);
+        });
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import Controller from "../controllers/controller";
 const routes = Router();
 routes.get("/", Controller.init);
-routes.get("/all", Controller.getAllUsers);
 routes.get("/loginhr", Controller.loginHR);
+routes.get("/getHR", Controller.getHR);
+routes.get("/getAllHR", Controller.getAllHR);
 export default routes;
