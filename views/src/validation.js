029a54c283de057db7ab2c92ce393cf3949c6ccd
Lina Sabboula <linamuhab@gmail.com>
2019-12-15 22:37:43 +0200

    Added initial AJAX function that retrieves specific data and sends form data

--- views/src/validation.js
+++ views/src/validation.js
@@ -2,39 +2,59 @@
 function validateEmailFormat(form) {
     var email = document.getElementById("email").value;
     var format = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(!format.test(email)){
+    if (!format.test(email)) {
         alert("Invalid email format!");
         return false;
         // form.email.focus();
     }
     return true;
 }
+
 //Makes sure that there are no empty fields
 function checkEmptyFields() {
-        let username = document.forms["form"]["username"].value;
-        let password = document.forms["form"]["password"].value;
-        if (username === "" && password === "") {
-            alert("please fill the empty fields!");
-            return false;
-        }
-        if (username === "" && password !=="") {
-            alert("please enter valid username");
-            return false;
-        }
-
-        if (password === "" && username !=="") {
-            alert("please enter a password");
-            return false;
-        }
+    let username = document.forms["form"]["username"].value;
+    let password = document.forms["form"]["password"].value;
+    if (username === "" && password === "") {
+        alert("please fill the empty fields!");
+        return false;
+    }
+    if (username === "" && password !== "") {
+        alert("please enter valid username");
+        return false;
     }
 
-$(".txtb input").on("focus",function(){
+    if (password === "" && username !== "") {
+        alert("please enter a password");
+        return false;
+    }
+    return true;
+}
+
+$(".txtb input").on("focus", function () {
     $(this).addClass("focus");
-}).on("blur",function(){
-    if($(this).val() === "")
+}).on("blur", function () {
+    if ($(this).val() === "")
         $(this).removeClass("focus");
 });
 
+$("#my-form").submit(function (e) {
+    e.preventDefault();
+    let initValidation = checkEmptyFields();
+    if (initValidation) {
+        $.ajax({
+            url: '/getHR', //url that will get data from DB
+            type: 'get',
+            data: $('my-form').serialize(), //form data
+            dataType: 'json',
+            success: function (res) {
+                alert("success!" + res); ///logic for checking
+            },
+            error: function (err) {
+                alert("Error:" + err.message);
+            }
+        });
+    }
+});
 
 
 
