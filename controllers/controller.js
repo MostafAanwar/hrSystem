37f45a471d48fc2bb73499c704036dd456840522
Lina Sabboula <linamuhab@gmail.com>
2019-12-15 19:07:18 +0200

    Added endpoint route to redirect to HR login page

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,28 +1,42 @@
 import model from "../model/model.js";
+
 const Path = require("path");
 var fs = require("fs");
 
 class Controller {
-  init(req, res) {
-    let path = Path.join(__dirname, "../views/index.html");
-    fs.readFile(path, function(err, html) {
-      if (err) {
-        throw err;
-      }
-      res.writeHeader(200, { "Content-Type": "text/html" });
-      res.write(html);
-      res.end();
-    });
-  }
-
-  getAllUsers(req, res) {
-    let users = model.getAllUsers();
-    if (users) {
-      res.status(200).send({
-        users: users
-      });
+    init(req, res) {
+        let path = Path.join(__dirname, "../views/index.html");
+        fs.readFile(path, function (err, html) {
+            if (err) {
+                throw err;
+            }
+            res.writeHeader(200, {"Content-Type": "text/html"});
+            res.write(html);
+            res.end();
+        });
     }
-  }
+
+    loginHR(req, res) {
+        let path = Path.join(__dirname, "../views/hrlogin.html");
+        fs.readFile(path, function (err, html) {
+            if (err) {
+                throw err;
+            }
+            res.writeHeader(200, {"Content-Type": "text/html"});
+            res.write(html);
+            res.end();
+        });
+    }
+
+    getAllUsers(req, res) {
+        let users = model.getAllUsers();
+        if (users) {
+            res.status(200).send({
+                users: users
+            });
+        }
+    }
 }
+
 const mainController = new Controller();
 export default mainController;
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ import Controller from "../controllers/controller";
 const routes = Router();
 routes.get("/", Controller.init);
 routes.get("/all", Controller.getAllUsers);
-
+routes.get("/loginhr", Controller.loginHR);
 export default routes;
