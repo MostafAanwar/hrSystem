fed751cf2250ba5453a4be15d5690a9f02cc4b9b
Lina Sabboula <linamuhab@gmail.com>
2019-12-15 22:33:52 +0200

    Added model query class structure

--- model/model.js
+++ model/model.js
@@ -1,7 +1,63 @@
-// import db from "../db/db";
-//
-// module.exports = {
-//   getAllUsers: function() {
-//     return db;
-//   }
-// };
+let mysql = require('mysql');
+
+class Model {
+    createConnectionPool() {
+        let connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'hrsystem',
+            port: 3311
+        });
+        return connection;
+    } // end connect
+
+    disconnect(connection) {
+        connection.end((err) => {
+            if (err)
+                return err.message;
+        });
+    }
+
+    queryFunction(sql, values) {
+        return new Promise(((resolve, reject) => {
+            let connection = this.createConnectionPool();
+            connection.connect(function (err) {
+                if (err) {
+                    console.log("Error: " + err.message);
+                    reject(err);
+                }
+                else {
+                    console.log("Connection established!");
+                }
+            });
+            connection.query(sql, values, (err, result) => {
+                console.log(result);
+                console.log(values);
+                resolve({ //return as json
+                    result: result,
+                    connection: connection
+                });
+            });
+        }));
+    }
+
+    getHR(email, password) {
+        let sql = 'SELECT * FROM hr where email = ? AND password = ?';
+        return this.queryFunction(sql, [email, password]);
+    }
+
+    getAllHR() {
+        let sql = "SELECT * FROM hr";
+        return this.queryFunction(sql, "");
+
+    }
+
+    getAllPositions() {
+        let sql = "SELECT * FROM position";
+        return this.queryFunction(sql, "");
+    }
+}
+
+const mainModel = new Model();
+export default mainModel;
