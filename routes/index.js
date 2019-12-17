fe4ce0feaab14144f64d224e7f5989af45031245
HabibaEsmail <habibaesmail@yahoo.com>
2019-12-17 17:31:07 +0200

    Added Cookie and Session function

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import Controller from "../controllers/controller";
 const routes = Router();
+
 routes.get("/", Controller.init);
 routes.get("/loginhr", Controller.loginHR);
+router.get('/loginhr', function(req , res , next){
+    req.session.favColor = 'Red';
+    res.send('Setting favourite color ...!');
+});
+
+router.get('/getColor', function(req , res , next){
+    res.send('Favourite Color : ' + (req.session.favColor == undefined?"NOT FOUND":req.session.favColor));
+});
 routes.get("/homePage",Controller.viewHomePage);
 routes.get("/Positions",Controller.viewPositions);
 routes.post("/getHR", Controller.getHR);
