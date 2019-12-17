77fc1cf04a9b38a3cd6b8e47f4161ec4823808fc
Lina Sabboula <linamuhab@gmail.com>
2019-12-17 19:52:52 +0200

    Added get user endpoint url and user homepage, temporarily commented out session route

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,18 @@ const routes = Router();
 
 routes.get("/", Controller.init);
 routes.get("/login-hr", Controller.loginHR);
-router.get('/login-hr', function(req , res , next){
-    req.session.favColor = 'Red';
-    res.send('Setting favourite color ...!');
-});
-
-router.get('/getColor', function(req , res , next){
-    res.send('Favourite Color : ' + (req.session.favColor == undefined?"NOT FOUND":req.session.favColor));
-});
-routes.get("/home-page",Controller.viewHomePage);
+// routes.get('/login-hr', function(req , res , next){
+//     req.session.favColor = 'Red';
+//     res.send('Setting favourite color ...!');
+// });
+//
+// routes.get('/getColor', function(req , res , next){
+//     res.send('Favourite Color : ' + (req.session.favColor == undefined?"NOT FOUND":req.session.favColor));
+// });
+routes.get("/home-page",Controller.userHomePage);
+routes.get("/hr-index", Controller.HRHomePage);
 routes.get("/positions",Controller.viewPositions);
 routes.post("/get-hr", Controller.getHR);
+routes.post('/get-user', Controller.getUser);
 routes.get("/get-all-hr", Controller.getAllHR);
 export default routes;
