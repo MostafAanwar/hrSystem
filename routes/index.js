ca133b6f0659c422e483db9367c4d800a19653e7
Habiba <habibaesmail@yahoo.com>
2019-12-18 22:41:21 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,13 @@ const routes = Router();
 
 routes.get("/", Controller.init);
 routes.get("/login-hr", Controller.loginHR);
-// routes.get('/login-hr', function(req , res , next){
-//     req.session.favColor = 'Red';
-//     res.send('Setting favourite color ...!');
-// });
-//
-// routes.get('/getColor', function(req , res , next){
-//     res.send('Favourite Color : ' + (req.session.favColor == undefined?"NOT FOUND":req.session.favColor));
-// });
 routes.get("/home-page",Controller.userHomePage);
 routes.get("/hr-index", Controller.HRHomePage);
-routes.get("/positions",Controller.getAllPositions);
+routes.get("/get-positions", Controller.GetPositionPage);
+routes.get("/positions",Controller.viewPositions);
 routes.post("/get-hr", Controller.getHR);
 routes.post('/get-user', Controller.getUser);
 routes.get("/get-all-hr", Controller.getAllHR);
 routes.get("/sign-up", Controller.signUp);
-//routes.post('/add-user', Controller.adduser);
 export default routes;
+
