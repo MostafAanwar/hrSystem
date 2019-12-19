e641b904f3357f84e8e73017a7c1cdd1b98c9fce
Lina Sabboula <linamuhab@gmail.com>
2019-12-19 23:39:47 +0200

    Edit position functionality

---
+++ views/src/edit-pos-script.js
@@ -0,0 +1,54 @@
+$(document).ready( function () {
+    let pageURL = window.location.href;
+    let PID = pageURL.substr(pageURL.lastIndexOf('=') + 1);
+    $.ajax({
+        url: "/get-pos",
+        type: "post",
+        data:{
+            PID: PID
+        },
+        dataType: 'json',
+        success: function (res) {
+            if (res[0] !== "") {
+                $('#title').val(res[0]['title']);
+                $('#description').val(res[0]['description']);
+                $('#salary').val(res[0]['salary']);
+                $('#available').prop("checked", res[0]['available']);
+            }
+        },
+        error: function (err) {
+            alert("Error:" + err.message);
+        }
+
+    })
+});
+
+$("#edit-pos-form").submit(function (e) {
+    let pageURL = window.location.href;
+    let PID = pageURL.substr(pageURL.lastIndexOf('=') + 1);
+    e.preventDefault();
+    $.ajax({
+        url: "/edit-pos", //url that will get data from DB
+        type: 'post',
+        data: {
+            PID: PID,
+            title: $('#title').val(),
+            description:$('#description').val() ,
+            available: $('#available').prop('checked'),
+            salary: $('#salary').val()
+            }, //form data
+        dataType: 'json',
+        success: function (res) { ///logic for checking
+            if(res['affectedRows'] === 1){
+                console.log("Succesfully edited position " + PID);
+                window.location.replace('/get-positions');
+            }
+        },
+        error: function (err) {
+            alert("Error:" + err.message);
+        }
+    });
+
+});
+
+
